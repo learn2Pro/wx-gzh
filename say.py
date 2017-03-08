bff92513281e2fb35d30dd5de863d3c703cf7159
tang <tyqw9110@qq.com>
2017-03-08 20:13:48 +0800

    1.poem 接口接入redis  2.0

--- say.py
+++ say.py
@@ -4,9 +4,16 @@
 import hashlib
 import web
 from poem import sample
+import redis
 
 
 class Poem(object):
+    def con(self):
+        pool = redis.ConnectionPool(host='23.106.148.101', port=6379, password=r'banwagong-redis')
+        r = redis.Redis(connection_pool=pool)
+        r.ttl(1000)
+        return r
+
     def GET(self):
         try:
             title = web.input()
